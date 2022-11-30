689089e94e64c616337d8a30174ab10ebd858c24
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-29 23:24:54 -0300

    style: implements Numbers UI screen

--- lib/screens/numbers.dart
+++ lib/screens/numbers.dart
@@ -10,6 +10,35 @@ class Numbers extends StatefulWidget {
 class _NumbersState extends State<Numbers> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/1.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/2.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/3.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/4.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/5.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/6.png"),
+        ),
+      ],
+    );
   }
 }
