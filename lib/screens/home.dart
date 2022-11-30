8626802ba1db41b8c764555aba5155485d084f32
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-29 22:45:07 -0300

    style: implements the initial UI structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:learn_english/screens/animals.dart';
+import 'package:learn_english/screens/numbers.dart';
+import 'package:learn_english/screens/vowels.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn English"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: "Animals",
+            ),
+            Tab(
+              text: "Numbers",
+            ),
+            Tab(
+              text: "Vowels",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Animals(),
+          Numbers(),
+          Vowels(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/numbers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Numbers extends StatefulWidget {
+  const Numbers({Key? key}) : super(key: key);
+
+  @override
+  State<Numbers> createState() => _NumbersState();
+}
+
+class _NumbersState extends State<Numbers> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
